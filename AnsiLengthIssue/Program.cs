8771ccfd66aa0740fa07f522625ce100542b9739
Sparin <sparin285@gmail.com>
2022-09-24 17:12:18 +0300

    Add project files.

---
+++ AnsiLengthIssue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.DirectoryServices.Protocols;
+using System.Net;
+
+var credentials = new NetworkCredential("CN=user01,OU=users,DC=example,DC=org", "Зфыы123;");
+//var credentials = new NetworkCredential("CN=user02,OU=users,DC=example,DC=org", "Pass123$");
+//var credentials = new NetworkCredential("user02", "Pass123$");
+
+var identifier = new LdapDirectoryIdentifier("localhost", 1389);
+using var connection = new LdapConnection(identifier, credentials, AuthType.Basic);
+connection.SessionOptions.ProtocolVersion = 3;
+connection.Bind(); // Throws Invalid Credentials Error
+
+Console.WriteLine("Successful bind");
